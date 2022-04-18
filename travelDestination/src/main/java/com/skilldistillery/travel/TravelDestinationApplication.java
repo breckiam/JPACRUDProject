09d9acdf8ecddf0d6d9f39718b0b79e5abd6f04d
breckiam <breckiam@gmail.com>
2022-04-18 11:06:04 -0600

    link fixes

--- travelDestination/src/main/java/com/skilldistillery/travel/TravelDestinationApplication.java
+++ travelDestination/src/main/java/com/skilldistillery/travel/TravelDestinationApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelDestinationApplication {
+public class TravelDestinationApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TravelDestinationApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelDestinationApplication.class, args);
